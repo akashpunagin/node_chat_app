4f2039712457feecf2e5752103c54f6a7eb32a8e
akashpunagin <akash.punagin@gmail.com>
2020-10-16 21:25:46 +0530

    emit message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log("Connected to server");
-  socket.emit('createMessage', {
-    from: "mike@gmail.com",
-    text: "Hello this is new message form mike"
-  })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -22,12 +22,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log("create message:", message);
-  });
-
-  socket.emit('newMessage', {
-    from: "example@gmail.com",
-    text: "Hello",
-    createdAt: new Date()
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date(),
+    });
   });
 });
 
